1835dcf7979a337b2a1a4676089bda89ad04dbd0
Pawan <imrealpawan@gmail.com>
2025-07-18 16:41:31 +0530

    middleware created

---
+++ app/login/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter()
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>)=>{
+     e.preventDefault();
+
+    const result =  await signIn("credentials",{
+        email,
+        password,
+        redirect: false
+     })
+
+     if(result?.error){
+      console.log(result.error)
+     }else{
+      router.push("/")
+     }
+  }
+  return <>
+  <h1>Login</h1>
+  <form onSubmit={handleSubmit}>
+   <input type="email" placeholder="Enter email"  value={email} onChange={(e)=> setEmail(e.target.value)} />
+   <input type="password" placeholder="Enter password"  value={password} onChange={(e)=> setPassword(e.target.value)} />
+   <button type="submit" >Login</button>
+  </form>
+ 
+  <div>
+   Don't have an account?
+   <button onClick={()=>router.push("/register")}>Register</button>
+  </div>
+  </>;
+};
+
+export default LoginPage;
