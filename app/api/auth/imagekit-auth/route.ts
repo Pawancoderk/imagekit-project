d9db0d25811d6b652505ed50a52ea93f35638c62
Pawan <imrealpawan@gmail.com>
2025-07-19 11:57:50 +0530

    File upload and api-client created

--- app/api/auth/imagekit-auth/route.ts
+++ app/api/auth/imagekit-auth/route.ts
@@ -1,10 +1,6 @@
 import { getUploadAuthParams } from "@imagekit/next/server";
 
 export async function GET() {
-  // Your application logic to authenticate the user
-  // For example, you can check if the user is logged in or has the necessary permissions
-  // If the user is not authenticated, you can return an error response
-
   try {
     const authenticationParameter = getUploadAuthParams({
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY as string,
